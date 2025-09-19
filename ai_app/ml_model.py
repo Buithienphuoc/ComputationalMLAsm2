fec2dc8746dbc352e6f0578468815e1dfca52b3c
Phuoc Bui <37607399+Buithienphuoc@users.noreply.github.com>
2025-09-19 12:17:10 +0700

    Update ml_model.py delete command outs

--- ai_app/ml_model.py
+++ ai_app/ml_model.py
@@ -11,24 +11,6 @@ def _load_model():
     if _model is None:
         _model = joblib.load(MODEL_PATH)
     return _model
-
-# def predict(data):
-#     model = _load_model()
-#     # if isinstance(data, dict):
-#     #     values = [data[k] for k in sorted(data.keys())]
-#     #     X = [values]
-#     # elif isinstance(data, list):
-#     #     if len(data) > 0 and isinstance(data[0], (list, tuple)):
-#     #         X = data
-#     #     else:
-#     #         X = [data]
-#     # else:
-#     #     X = [[data]]
-#     result = [model.predict(data)]
-#     try:
-#         return result.tolist()
-#     except Exception:
-#         return list(result)
     
 def predict(data):
     """
