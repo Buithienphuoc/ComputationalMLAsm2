9d5a884e35a466ce366fa064066b4b051c7980be
Phuoc Bui <37607399+Buithienphuoc@users.noreply.github.com>
2025-09-19 12:40:09 +0700

    Update settings.py try no SSL

--- ai_project/settings.py
+++ ai_project/settings.py
@@ -68,8 +68,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 if DATABASE_URL:
     DATABASES["default"] = dj_database_url.parse(
         DATABASE_URL,
-        conn_max_age=600,   # giữ connection lâu hơn để tiết kiệm
-        ssl_require=True    # Railway/Heroku thường cần SSL
+        conn_max_age=600   # giữ connection lâu hơn để tiết kiệm 
     )
 
 CSRF_TRUSTED_ORIGINS = [
